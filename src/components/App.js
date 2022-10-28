ba9052340997dd3391eabbe3a82865361d58af23
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-10-28 19:45:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div id="main">
       // Do not alter the main div
+      {getFlag && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id"click" onClick={()=>setFlag(!getFlag)}>click</button>
     </div>
   );
 }
